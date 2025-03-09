7f7b9fb09d4abd2697f99ee4f6af7c55d9c5fe72
vincent44-art <vincent.murithi@student.moringaschoo.com>
2025-03-09 11:56:03 +0300

    commit

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-const name = "Joe";
-const height = 74;
+
+// index.js
+const name = "Susan"; // Changed from "Joe"
+const height = 35;    // Changed from 74 (must be < 40)
 const message = `${name} is ${height} inches tall`;
 
-module.exports = {name, height, message};
+module.exports = { name, height, message };
